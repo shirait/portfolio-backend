2ad2d0fe36fba2054518235721d9679a7e198171
shirait <hamburgkarry@gmail.com>
2026-05-13 18:02:32 +0900

    ログアウト時、「NoMethodError (undefined method 'respond_to' for an instance of Devise::SessionsController):」が出ないようにした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  # 「NoMethodError (undefined method 'respond_to' for an instance of Devise::SessionsController):」の対策
+  include ActionController::MimeResponds
 end

1df38e3a4bae4e89dbcd65e8f8d2f4ebe9934894
shirait <hamburgkarry@gmail.com>
2026-05-14 10:50:12 +0900

    enumのnewという名称が予約語と重複していたので別のものに置き換え

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :user
-  enum status: { new: 0, in_progress: 1, resolved: 2, completed: 3, feedback: 4, rejected: 5 }
+  enum :status, { not_started: 0, in_progress: 1, resolved: 2, completed: 3, feedback: 4, rejected: 5 }
 end
